675e0bc8650f56cbd43769571cb9e930c9f5745d
Şahin Əliyarlı <74450071+sliyarli@users.noreply.github.com>
2023-09-08 14:43:47 +0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,17 @@
+// Function to validate an email address using a regular expression
 function isValidEmail(email) {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return emailRegex.test(email);
 }
+
+// Function to get error elements associated with an input
 function getErrorElements(inputElement) {
   const errorText = inputElement.parentElement.nextElementSibling;
   const errorIcon = inputElement.nextElementSibling;
   return { errorText, errorIcon };
 }
+
+// Function to display an error message and icon for an input field
 function displayError(inputElement, errorMessage) {
   const { errorText, errorIcon } = getErrorElements(inputElement);
   errorText.textContent = errorMessage;
@@ -14,6 +19,8 @@ function displayError(inputElement, errorMessage) {
   errorIcon.style.opacity = "1";
   inputElement.style.borderColor = "var(--primary-red)";
 }
+
+// Function to remove the error message and icon for an input field
 function removeError(inputElement) {
   const { errorText, errorIcon } = getErrorElements(inputElement);
   errorText.textContent = "";
@@ -21,8 +28,11 @@ function removeError(inputElement) {
   errorIcon.style.opacity = "0";
   inputElement.style.borderColor = "var(--neutral-grayish-blue)";
 }
+
+// Function to validate an input with a custom validation function
 function validateInput(inputElement, validationFunction, errorMessage) {
   const value = inputElement.value.trim();
+
   if (!validationFunction(value)) {
     displayError(inputElement, errorMessage);
     return false;
@@ -31,42 +41,9 @@ function validateInput(inputElement, validationFunction, errorMessage) {
     return true;
   }
 }
-function validateForm(event) {
-  event.preventDefault();
-  const firstNameInput = document.getElementById("firstName");
-  const lastNameInput = document.getElementById("lastName");
-  const emailInput = document.getElementById("email");
-  const passwordInput = document.getElementById("password");
-  const isFirstNameValid = validateInput(
-    firstNameInput,
-    (value) => value !== "",
-    "First Name cannot be empty"
-  );
-  const isLastNameValid = validateInput(
-    lastNameInput,
-    (value) => value !== "",
-    "Last Name cannot be empty"
-  );
-  const isEmailValid = validateInput(
-    emailInput,
-    isValidEmail,
-    "Looks like this is not an email"
-  );
-  const isPasswordValid = validateInput(
-    passwordInput,
-    (value) => value !== "",
-    "Password cannot be empty"
-  );
-  if (isFirstNameValid && isLastNameValid && isEmailValid && isPasswordValid) {
-    alert("All inputs are valid! Form can be submitted.");
-  }
-}
-const form = document.querySelector("form");
-form.addEventListener("submit", validateForm);
+
+// Add input event listeners to trigger real-time validation for First Name
 const firstNameInput = document.getElementById("firstName");
-const lastNameInput = document.getElementById("lastName");
-const emailInput = document.getElementById("email");
-const passwordInput = document.getElementById("password");
 firstNameInput.addEventListener("input", () => {
   validateInput(
     firstNameInput,
@@ -74,6 +51,9 @@ firstNameInput.addEventListener("input", () => {
     "First Name cannot be empty"
   );
 });
+
+// Add input event listeners to trigger real-time validation for Last Name
+const lastNameInput = document.getElementById("lastName");
 lastNameInput.addEventListener("input", () => {
   validateInput(
     lastNameInput,
@@ -81,9 +61,15 @@ lastNameInput.addEventListener("input", () => {
     "Last Name cannot be empty"
   );
 });
+
+// Add input event listeners to trigger real-time validation for Email
+const emailInput = document.getElementById("email");
 emailInput.addEventListener("input", () => {
   validateInput(emailInput, isValidEmail, "Looks like this is not an email");
 });
+
+// Add input event listeners to trigger real-time validation for Password
+const passwordInput = document.getElementById("password");
 passwordInput.addEventListener("input", () => {
   validateInput(
     passwordInput,
@@ -91,3 +77,45 @@ passwordInput.addEventListener("input", () => {
     "Password cannot be empty"
   );
 });
+
+// Function to validate the form on submission
+function validateForm(event) {
+  event.preventDefault();
+
+  const firstNameInput = document.getElementById("firstName");
+  const lastNameInput = document.getElementById("lastName");
+  const emailInput = document.getElementById("email");
+  const passwordInput = document.getElementById("password");
+
+  const isFirstNameValid = validateInput(
+    firstNameInput,
+    (value) => value !== "",
+    "First Name cannot be empty"
+  );
+
+  const isLastNameValid = validateInput(
+    lastNameInput,
+    (value) => value !== "",
+    "Last Name cannot be empty"
+  );
+
+  const isEmailValid = validateInput(
+    emailInput,
+    isValidEmail,
+    "Looks like this is not an email"
+  );
+
+  const isPasswordValid = validateInput(
+    passwordInput,
+    (value) => value !== "",
+    "Password cannot be empty"
+  );
+
+  if (isFirstNameValid && isLastNameValid && isEmailValid && isPasswordValid) {
+    alert("All inputs are valid! Form can be submitted.");
+  }
+}
+
+// Add a submit event listener to the form to trigger form validation
+const form = document.querySelector("form");
+form.addEventListener("submit", validateForm);
